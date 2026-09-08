6d95fcdfc741b45288093904a863fd49e1efa5b1
lamiskin <lamiskin@users.noreply.github.com>
2026-09-08 05:21:19 +0000

    chore(main): release 0.1.2 (#13)

--- pyegp_parser/__init__.py
+++ pyegp_parser/__init__.py
@@ -10,7 +10,7 @@ from .models.bulk import BulkResult
 from .models.log_code import LogElement
 from .models.project import ParsedProject
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 logger = logging.getLogger(__name__)
 
